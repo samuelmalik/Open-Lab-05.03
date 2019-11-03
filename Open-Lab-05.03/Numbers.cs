5c4ad0129bb06c6ee359a44dd7aaea36463f2b5e
Erik Zemčík <zemcik.erik@gmail.com>
2019-11-03 15:43:08 +0100

    Created tests

---
+++ Open-Lab-05.03/Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_05._03
+{
+    public class Numbers
+    {
+        public string FormatNum(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-05.03/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Open_Lab_05._03
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Numbers numbers;
+
+        private const int RandSeed = 503503503;
+        private const int RandTestCasesCount = 97;
+
+        [OneTimeSetUp]
+        public void Init() => numbers = new Numbers();
+
+        [TestCase(1000, "1,000")]
+        [TestCase(100000, "100,000")]
+        [TestCase(20, "20")]
+        [TestCaseSource(nameof(GetRandom))]
+        public void FormatNumTest(int num, string expected) =>
+            Assert.That(numbers.FormatNum(num), Is.EqualTo(expected));
+
+        private static IEnumerable GetRandom()
+        {
+            var rand = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var num = rand.Next();
+                yield return new TestCaseData(num, num.ToString("N0"));
+            }
+        }
+
+    }
+}
