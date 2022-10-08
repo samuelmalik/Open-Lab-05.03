aac1b2d100ad37c8338a53e90fa3b9ce7ad6249a
samuelmalik <samuelmalik9@gmail.com>
2022-10-08 22:21:26 +0200

    added function formating numbers

--- Open-Lab-05.03/Numbers.cs
+++ Open-Lab-05.03/Numbers.cs
@@ -6,7 +6,18 @@ namespace Open_Lab_05._03
     {
         public string FormatNum(int num)
         {
-            throw new NotImplementedException();
+            string number = num.ToString();
+            if (number.Length > 3 && number.Length < 7)
+            {
+                number = number.Insert(number.Length - 3, ",");
+                return number;
+            }
+            if (number.Length >= 7)
+            {
+                number = string.Format("{0:#,0}", num);
+            }
+            
+                return number;
         }
     }
 }
